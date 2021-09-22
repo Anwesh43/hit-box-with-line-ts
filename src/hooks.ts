ed797130eefaf3520efbad23c222f4eec66815ac
Anwesh43 <anweshthecool0@gmail.com>
2021-09-22 17:19:35 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, CSSProperties} from 'react'
 
 const scGap : number = 0.02 
 const delay : number = 20 
@@ -36,12 +36,62 @@ export const useDimension = () => {
         }
         return () => {
             window.onresize = () => {
-                
+
             }
         }
     }, [])
     return {
         w, 
         h
+    }
+}
+
+const maxScale = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+const sinify = (scale : number) : number => Math.sin(scale * Math.PI)
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const position = 'absolute'
+    const size = Math.min(w, h) / 10
+    const background = 'indigo'
+    const sf = sinify(scale)
+    return {
+        parentStyle() : CSSProperties {
+            const left = `${w / 2}px`
+            const top = `${h / 2}px`
+            return {
+                position, 
+                left, 
+                top
+            }
+        },
+        lineStyle() : CSSProperties {
+            const sf1 : number = divideScale(sf, 0, 2)
+            const left = `${w /2 + w * 0.5 * sf1}px`
+            const width = `${size}px`
+            const height = `${Math.min(w, h) / 90}px`
+            return {
+                position, 
+                left, 
+                width, 
+                height,
+                background 
+            }
+        },
+        barStyle() : CSSProperties {
+            const sf2 : number = divideScale(sf, 1, 2)
+            const left = `${-size - (w / 2 - size) * sf2}px`
+            const top = `${-size / 2}px`
+            const height = `${size}px`
+            const width = `${size}px`
+            return {
+                position, 
+                background, 
+                left, 
+                top, 
+                width, 
+                height 
+            }
+        }
     }
 }
